ed0a197cf32b8867d4defd74e734bf3b58c11b1e
yuxuan.zhuang@dbb.su.se <yuxuan.zhuang@dbb.su.se>
2022-05-17 22:16:09 +0200

    add doc to test

---
+++ tests/README.rst
@@ -0,0 +1,5 @@
+============================
+Ensemble Parallel MDAnalysis
+============================
+
+Test package
--- tests/setup.py
+++ tests/setup.py
@@ -6,10 +6,10 @@ from setuptools import setup, find_packages
 
 RELEASE='0.4.0'
 
-with open('../README.rst') as readme_file:
+with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-with open('../HISTORY.rst') as history_file:
+with open('HISTORY.rst') as history_file:
     history = history_file.read()
     
 setup(
